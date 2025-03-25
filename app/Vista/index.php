64b88946b40f84d833300f992e02624df121169b
YordyHc <yordyhuamanc@gmail.com>
2025-03-25 15:42:31 -0500

    feat: agregar base de opciones de widget

--- app/Vista/index.php
+++ app/Vista/index.php
@@ -3,14 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/creacion_widgets_youtube/style/index.css">
     <title>Gallery Youtube</title>
 </head>
-<body>
-    <div class="container">
-        <div class="container border">
-            opciones
+<body><br>
+    <div class="ven_2 container-fluid rounded-4">
+        <div class="opciones container border ">
+            <div class="head_opcion container">
+                <p>Seleccione una platilla</p>
+            </div>
+            <div class="opcion container border">
+                <img src="https://placehold.co/150x150" class="img-fluid" alt="opcion_1">
+                <p>galeria</p>
+            </div>
+            <div class="opcion container border" >
+                <img src="https://placehold.co/150x150/aa0000/FFF" class="img-fluid" alt="opcion_2">
+                <p>galeria</p>
+            </div>
+            <div class="opcion container">
+                <img src="https://placehold.co/150x150/aacc00/FFF"  class="img-fluid" alt="opcion_3">
+                <p>galeria</p>    
+            </div>
+            <div class="opcion container">
+                <img src="https://placehold.co/150x150/ecac00/000" class="img-fluid" alt="opcion_4">    
+                <p>galeria</p>
+            </div>
         </div>
-        <div class="container border">muestras</div>
+        <div class="muestras container border">muestras</div>
     </div>
 </body>
 </html>
