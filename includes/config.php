8a864f07df150eaded776f6ceda6fe34c84737ed
YordyHc <yordyhuamanc@gmail.com>
2025-11-28 16:27:16 -0500

    feat: cambio en config

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-define('API_KEY', 'ingresa_clave_api');
+define('API_KEY', 'ingresa_la_api_key');
 /*
 id de canal de prueba 1: UCCfPkh8osJPC2pPq283kKXg
 id de canal de prueba 2: UCsMzBY3X_RS6GhGBiHXW19A
